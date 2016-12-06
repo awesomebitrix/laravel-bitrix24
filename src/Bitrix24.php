8c765900f78726eed43d020b09a803e0bb034225
Sergey Antsupov <s.antsupov@e-mba.ru>
2016-12-06 14:30:59 +0300

    -add get access token

--- src/Bitrix24.php
+++ src/Bitrix24.php
@@ -29,13 +29,14 @@ class Bitrix24
         $this->bitrix24->setApplicationScope($this->makeScope($this->config('B24_APPLICATION_SCOPE')));
         $this->bitrix24->setApplicationId($this->config('B24_APPLICATION_ID'));
         $this->bitrix24->setApplicationSecret($this->config('B24_APPLICATION_SECRET'));
-        $accessToken = $this->getAccessToken();
+
         if (!empty($this->config('REDIRECT_URL'))) {
             $this->bitrix24->setRedirectUri($this->config('REDIRECT_URL'));
         }
         if (!empty($this->config('DOMAIN'))) {
             $this->bitrix24->setDomain($this->config('DOMAIN'));
         }
+        $accessToken = $this->getAccessToken();
         if (!empty($this->config('MEMBER_ID'))) {
             $this->bitrix24->setMemberId($accessToken->member_id);
         }
