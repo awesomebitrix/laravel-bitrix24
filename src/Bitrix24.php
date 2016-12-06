31a108e42bc1a8be23a3819de42b810a5e08fed9
Sergey Antsupov <s.antsupov@e-mba.ru>
2016-12-06 08:40:01 +0300

    - fix scope

--- src/Bitrix24.php
+++ src/Bitrix24.php
@@ -1,7 +1,7 @@
 <?php
 namespace Antsupovsa\Bitrix24;
 
-//use Bitrix24\CRM\Lead;
+use Bitrix24\CRM\Lead;
 use Illuminate\Support\Arr;
 
 class Bitrix24
@@ -28,20 +28,20 @@ class Bitrix24
 
         // init lib
         $this->bitrix24 = new \Bitrix24\Bitrix24();
-        $this->bitrix24->setApplicationScope($this->config('B24_APPLICATION_SCOPE'));
+        $this->bitrix24->setApplicationScope(implode(",", $this->config('B24_APPLICATION_SCOPE')));
         $this->bitrix24->setApplicationId($this->config('B24_APPLICATION_ID'));
         $this->bitrix24->setApplicationSecret($this->config('B24_APPLICATION_SECRET'));
 
-        if(!empty($this->config('DOMAIN'))) {
+        if (!empty($this->config('DOMAIN'))) {
             $this->bitrix24->setDomain($this->config('DOMAIN'));
         }
-        if(!empty($this->config('MEMBER_ID'))) {
+        if (!empty($this->config('MEMBER_ID'))) {
             $this->bitrix24->setMemberId($this->config('MEMBER_ID'));
         }
-        if(!empty($this->config('AUTH_ID'))) {
+        if (!empty($this->config('AUTH_ID'))) {
             $this->bitrix24->setAccessToken($this->config('AUTH_ID'));
         }
-        if(!empty($this->config('REFRESH_ID'))) {
+        if (!empty($this->config('REFRESH_ID'))) {
             $this->bitrix24->setRefreshToken($this->config('REFRESH_ID'));
         }
     }
